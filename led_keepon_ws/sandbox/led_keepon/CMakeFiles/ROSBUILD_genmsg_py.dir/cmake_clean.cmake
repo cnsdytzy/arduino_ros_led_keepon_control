672cf0260bab586fdfd5ed6010e454a2c3d3e36a
Harihar Subramanyam <harihar.subramanyam@vecna.com>
2014-01-21 14:48:10 -0500

    Updated ws

---
+++ led_keepon_ws/sandbox/led_keepon/CMakeFiles/ROSBUILD_genmsg_py.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "msg_gen"
+  "src/led_keepon/msg"
+  "msg_gen"
+  "CMakeFiles/ROSBUILD_genmsg_py"
+  "src/led_keepon/msg/__init__.py"
+  "src/led_keepon/msg/_LEDMessage.py"
+  "src/led_keepon/msg/_KeeponMessage.py"
+  "src/led_keepon/msg/_ScenarioTrigger.py"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmsg_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
